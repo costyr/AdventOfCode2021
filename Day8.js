8dc83653749986b0c6a879cadbe4dd8b56a1c81d
Costi Rada <costi.rada@caphyon.com>
2021-12-08 12:31:04 +0200

    Work work

--- Day8.js
+++ Day8.js
@@ -11,7 +11,9 @@ function Analize(aSegments) {
 }
 
 function Decode(aEncodingMap) {
-     
+  for (let key in aEncodingMap) {
+    console.log(key + "=>" + aEncodingMap[key]);
+  }   
 }
 
 function Analize2(aSegments) {
@@ -34,24 +36,14 @@ function Analize2(aSegments) {
       else if (aSegments[i].l[j].length == 7) 
         encodingMap[ff] = { code: 8, p: [] };
     }
+
+    console.log(encodingMap);
     
     Decode(encodingMap);
   }              
 }
 
-function Analize3(aSegments) {
-  for (let i = 0; i < aSegments.length; i++) {
-
-    let tt = 0;
-    for (let j = 0; j < aSegments[i].l.length; j++)
-    {
-      let pp = aSegments[i].r[j].split('').sort().join();
-      tt += j 
-    }  
-  }              
-}
-
-let segments = util.MapInput('./Day8Input.txt', (aElem) => {
+let segments = util.MapInput('./Day8TestInput2.txt', (aElem) => {
   let raw = aElem.split(' | ');
   return { l: raw[0].split(' ').map(aElem => aElem.split('').sort().join('')), r: raw[1].split(' ').map(aElem => aElem.split('').sort().join(''))};
 }, '\r\n');
