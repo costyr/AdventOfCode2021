98feb36a3808fef671002f161e35a4ff5c1f8abe
Costi Rada <costi.rada@caphyon.com>
2021-12-13 12:37:38 +0200

    Day 13 solution

--- Day13.js
+++ Day13.js
@@ -1,99 +1,47 @@
 const util = require('./Util.js');
 const alg = require('./Matrix.js');
 
-function PrintPath(aPath) {
-  let line = '';
-  for (let i = 0; i < aPath.length; i++) {
-    if (line.length > 0)
-      line += ",";
-    line += aPath[i]
-
-  }
-  return line;
-}
-
-const isUpperCase = (string) => /^[A-Z]*$/.test(string);
-
-function CreateGraph(aNodeList) {
-  let graph = new alg.Graph();
-
-  for (let i = 0; i < aNodeList.length; i++) {
-    graph.AddNeighbour(aNodeList[i][0], aNodeList[i][1]);
-    graph.AddNeighbour(aNodeList[i][1], aNodeList[i][0]);
-  }
-
-  return graph;
-}
-
-function IsValidPath(aPath, aNode) {
-
-  if (!isUpperCase(aNode) && aPath.indexOf(aNode) != -1)
-    return false;
-
-  return true;
-}
-
-function IsValidPath2(aPath) {
-
-  let rr = [];
-  for (let i = 0; i < aPath.length; i++)
-    if (!isUpperCase(aPath[i]))
-      if (rr[aPath[i]] === undefined)
-        rr[aPath[i]] = 1;
-      else
-        rr[aPath[i]] += 1;
-
-  let tt = 0;
-  for (let key in rr) {
-    if (rr[key] > 2)
-      return false;
-    else if (rr[key] == 2)
-      tt++;
-  }
-
-  if (tt > 1)
-    return false;
-
-  return true;
-}
-
 function FoldY(aPaper, aPos) {
   for (let i = 0; i < aPaper.mMatix.length; i++)
-    for (let j = 0; j < aPaper.mMatix[i].length; j++)
-    {
-      if (i > aPos && aPaper.GetValue(i, j) == '#')
-      {
+    for (let j = 0; j < aPaper.mMatix[i].length; j++) {
+      if (i > aPos && aPaper.GetValue(i, j) == '#') {
         aPaper.SetValue(i, j, '.');
-        aPaper.SetValue(aPaper.mMatix.length - i - 1, j, '#');   
-      }  
+        aPaper.SetValue(aPaper.mMatix.length - i - 1, j, '#');
+      }
     }
+
+  aPaper.mMatix.splice(aPos);
 }
 
 function FoldX(aPaper, aPos) {
-  for (let i = 0; i < aPaper.mMatix.length; i++)
-    for (let j = 0; j < aPaper.mMatix[i].length; j++)
-    {
-      if (j > aPos && aPaper.GetValue(i, j) == '#')
-      {
+  for (let i = 0; i < aPaper.mMatix.length; i++) {
+    for (let j = 0; j < aPaper.mMatix[i].length; j++) {
+      if (j > aPos && aPaper.GetValue(i, j) == '#') {
         aPaper.SetValue(i, j, '.');
-        aPaper.SetValue(i, aPaper.mMatix[0].length - j - 1, '#');   
-      }  
+        aPaper.SetValue(i, aPaper.mMatix[0].length - j - 1, '#');
+      }
     }
+  }
+  for (let i = 0; i < aPaper.mMatix.length; i++)
+    aPaper.mMatix[i].splice(aPos);
 }
 
-function MarkPoints(aCoords) {
+function MarkPoints(aCoords, aFolds) {
 
   let maxX = 0;
   let maxY = 0;
-  for (let i = 0; i < aCoords.length; i++) {
-    if (aCoords[i].x > maxX)
-      maxX = aCoords[i].x;
-
-    if (aCoords[i].y > maxY)
-      maxY = aCoords[i].y;
+  for (let i = 0; i < aFolds.length; i++) {
+    if (aFolds[i].dir) {
+      if (aFolds[i].pos > maxX)
+        maxX = aFolds[i].pos;
+    }
+    else {
+      if (aFolds[i].pos > maxY)
+        maxY = aFolds[i].pos;
+    }
   }
 
-  let paper = new alg.Matrix(maxX + 1, maxY + 1, '.');
+  let paper = new alg.Matrix(maxX * 2 + 1, maxY * 2 + 1, '.');
 
   for (let i = 0; i < aCoords.length; i++)
     paper.SetValue(aCoords[i].y, aCoords[i].x, '#');
@@ -101,44 +49,34 @@ function MarkPoints(aCoords) {
   return paper;
 }
 
-/*function Fold(aPaper, aFolds) {
-  for (let i = 0; i < aFolds.length; i++)
-}*/
+function Fold(aPaper, aFolds, aFoldsCount) {
+  let firstFoldCount = 0
+  for (let i = 0; i < aFoldsCount; i++) {
+    if (aFolds[i].dir)
+      FoldX(aPaper, aFolds[i].pos);
+    else
+      FoldY(aPaper, aFolds[i].pos);
+
+    if (i == 0)
+      firstFoldCount = aPaper.CountElement('#');
+  }
+
+  return firstFoldCount;
+}
 
 let insts = util.MapInput('./Day13Input.txt', (aElem, aIndex) => {
 
-  if (aIndex == 0) {
-     return aElem.split('\r\n').map(a=>{ let rr = a.split(','); return {x: parseInt(rr[0]), y: parseInt(rr[1]) }; });
-  }
+  if (aIndex == 0)
+    return aElem.split('\r\n').map(a => { let rr = a.split(','); return { x: parseInt(rr[0]), y: parseInt(rr[1]) }; });
   else
-  {
-    return aElem.split('\r\n').map(a=>{ let rr = a.split('='); return { dir: (rr[0] == 'fold along x'), coord: parseInt(rr[1]) };});
-  }
+    return aElem.split('\r\n').map(a => { let rr = a.split('='); return { dir: (rr[0] == 'fold along x'), pos: parseInt(rr[1]) }; });
 
 }, '\r\n\r\n');
 
-console.log(insts[0]);
-console.log(insts[1]);
+let paper = MarkPoints(insts[0], insts[1]);
 
-let paper = MarkPoints(insts[0]);
+let firstFoldCount = Fold(paper, insts[1], insts[1].length);
 
-paper.Print('');
+console.log(firstFoldCount);
 
-//FoldY(paper, 7);
-
-//console.log('\n');
-
-//paper.Print('');
-
-FoldX(paper, 655);
-
-console.log('\n');
-
-paper.Print('');
-
-console.log(paper.CountElement('#'));
-
-//let gr = CreateGraph(nodeList);
-
-//console.log(BFS(gr, true));
-//console.log(BFS(gr, false));
+paper.Print('', (aElem) => { if (aElem == '.') return ' '; else return '*'; });
