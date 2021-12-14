907054498774f25cbf9955075fa610782048da63
Costi Rada <costi.rada@caphyon.com>
2021-12-14 11:52:44 +0200

    Work work

--- Day14.js
+++ Day14.js
@@ -1,39 +1,4 @@
 const util = require('./Util.js');
-const alg = require('./Matrix.js');
-
-function FoldY(aPaper, aPos) {
-  for (let i = 0; i < aPaper.mMatix.length; i++)
-    for (let j = 0; j < aPaper.mMatix[i].length; j++) {
-      if (i > aPos && aPaper.GetValue(i, j) == '#') {
-        aPaper.SetValue(i, j, '.');
-        aPaper.SetValue(aPos - (i - aPos), j, '#');
-      }
-    }
-
-  aPaper.mMatix.splice(aPos);
-}
-
-function FoldX(aPaper, aPos) {
-  for (let i = 0; i < aPaper.mMatix.length; i++) {
-    for (let j = 0; j < aPaper.mMatix[i].length; j++) {
-      if (j > aPos && aPaper.GetValue(i, j) == '#') {
-        aPaper.SetValue(i, j, '.');
-        aPaper.SetValue(i, aPos - (j - aPos), '#');
-      }
-    }
-  }
-  for (let i = 0; i < aPaper.mMatix.length; i++)
-    aPaper.mMatix[i].splice(aPos);
-}
-
-function MarkPoints(aCoords, aSize) {
-  let paper = new alg.Matrix(aSize.x + 1, aSize.y + 1, '.');
-
-  aCoords.map((aCoord)=>{
-    paper.SetValue(aCoord.y, aCoord.x, '#');}, this);
-
-  return paper;
-}
 
 function CreatePolimer(aTemplate, aInsts, aStepCount) {
 
@@ -49,7 +14,7 @@ function CreatePolimer(aTemplate, aInsts, aStepCount) {
       i++;
     }
   }
-  console.log(k);
+  console.log(aTemplate.join(''));
 }
 
   let oo = [];
@@ -74,7 +39,65 @@ function CreatePolimer(aTemplate, aInsts, aStepCount) {
   return max - min;
 }
 
-let insts = util.MapInput('./Day14Input.txt', (aElem, aIndex) => {
+function InvertRules(aInsts) {
+
+  let gg = []
+  for (let key in aInsts)
+  {
+    if (gg[aInsts[key]] === undefined)
+      gg[aInsts[key]] = [];
+    gg[aInsts[key]].push(key); 
+  } 
+  
+  return gg;
+}
+
+function CreatePolimer2(aTemplate, aInsts, aInvInsts, aStepCount) {
+
+  for (let k = 0; k < aStepCount; k++) {
+  for (let i = 0; i < aTemplate.length - 1; i++)
+  {
+    let pp = aTemplate[i] + aTemplate[i + 1];
+    let nn = aInsts[pp];
+
+    if (nn !== undefined)
+    {
+      aTemplate.splice(i + 1, 0, nn);
+      i++;
+    }
+  }
+
+  let last = aTemplate[aTemplate.length - 2] + aTemplate[aTemplate.length - 1];
+
+  if (aInvInsts['B'].indexOf(last) == -1 && aInvInsts['H'].indexOf(last) == -1)
+    aTemplate.splice(aTemplate.length - 1, 1);
+
+  console.log(k + " " + aTemplate.join(''));
+}
+
+  let oo = [];
+  for (let i = 0; i < aTemplate.length; i++)
+    if (oo[aTemplate[i]] === undefined)
+      oo[aTemplate[i]] = 0;
+    else
+      oo[aTemplate[i]] += 1;
+
+  let min = Number.MAX_SAFE_INTEGER;
+  let max = 0;
+
+  for (let key in oo)
+  {
+    if (oo[key] > max)
+      max = oo[key];
+    
+    if (oo[key] < min)
+      min = oo[key];
+  }
+
+  console.log(oo['B']  + " " + oo['H']);
+}
+
+let insts = util.MapInput('./Day14TestInput.txt', (aElem, aIndex) => {
 
   if (aIndex == 0) {
     return aElem.split('');
@@ -90,7 +113,25 @@ let insts = util.MapInput('./Day14Input.txt', (aElem, aIndex) => {
 
 console.log(insts);
 
-let ret = CreatePolimer(insts[0], insts[1], 40);
+console.log(insts[0].join(''));
+
+let invInsts = InvertRules(insts[1]);
+
+console.log(invInsts);
+
+//let ret = CreatePolimer(insts[0], insts[1], 10);
+
+//console.log(ret);
+
+ret = CreatePolimer2(['N', 'N'], insts[1], invInsts, 10);
+
+console.log(ret);
+
+ret = CreatePolimer2(['N', 'C'], insts[1], invInsts, 10);
+
+console.log(ret);
+
+ret = CreatePolimer2(['C', 'B'], insts[1], invInsts, 10);
 
 console.log(ret);
 
