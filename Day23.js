049d9f716c15dad4fd8ed7a46409468428ab65c5
Costi Rada <costi.rada@caphyon.com>
2021-12-24 22:17:49 +0200

    Work work

--- Day23.js
+++ Day23.js
@@ -7,21 +7,21 @@ const kAmphipods = ['A', 'B', 'C', 'D'];
 const kAmphipodsFinalPositions = [[{ x: 3, y: 2 }, 
                                    { x: 3, y: 3 }, 
                                    { x: 3, y: 4 }, 
-                                   { x: 3, y: 5 }],
+                                  { x: 3, y: 5 }],
                                   [{ x: 5, y: 2 }, 
                                    { x: 5, y: 3 }, 
                                    { x: 5, y: 4 }, 
-                                   { x: 5, y: 5 }],
+                                  { x: 5, y: 5 }],
                                   [{ x: 7, y: 2 }, 
                                    { x: 7, y: 3 }, 
                                    { x: 7, y: 4 }, 
-                                   { x: 7, y: 5 }],
+                                  { x: 7, y: 5 }],
                                   [{ x: 9, y: 2 }, 
                                    { x: 9, y: 3 }, 
                                    { x: 9, y: 4 }, 
-                                   { x: 9, y: 5 }]];
+                                  { x: 9, y: 5 }]];
 
-function InValidDirection(aValue) {
+function IsValidDirection(aValue) {
   return true;
 }
 
@@ -50,16 +50,24 @@ function IsOnAnyEndPosition(aPos) {
   return false;
 }
 
-function IsNoPosition(aPos, aMap) {
-  if (IsOnAnyEndPosition(aPos))
+function IsNoPosition(aPos, aEndPositions, aSimbol) {
+
+  if (((aPos.x == 3) && (aPos.y == 1)) ||
+      ((aPos.x == 5) && (aPos.y == 1)) ||
+      ((aPos.x == 7) && (aPos.y == 1)) ||
+      ((aPos.x == 9) && (aPos.y == 1))) 
     return true;
 
-  if (((aPos.y + 1) < aMap.length) &&
-    ((aPos.x - 1) >= 0) &&
-    ((aPos.x + 1) < aMap[0].length) &&
-    (aMap[aPos.y + 1][aPos.x - 1] == '#') &&
-    (aMap[aPos.y + 1][aPos.x + 1] == '#'))
+  if (IsOnAnyEndPosition(aPos) && !IsEndPosition(aPos, aEndPositions))
     return true;
+
+  if (IsEndPosition(aPos, aEndPositions))
+  {
+    let bottom = map[aPos.y + 1][aPos.x];
+              
+    if ((bottom != '#') && (bottom != aSimbol))
+      return true;
+  }
 
   return false;
 }
@@ -90,21 +98,23 @@ function GenerateEndNode(aMap) {
 
 class SpecialGraph {
 
-  constructor(aStartNode) {
+  constructor(aStartNode, aEndNode) {
     this.mStartNode = aStartNode;
-    this.mCache = [];
+    this.mEndNode = aEndNode;
   }
 
   ComputeCost(aNodeId) {
 
-    if (this.mCache[aNodeId] !== undefined) {
-      console.log("cache hit!");
-      return this.mCache[aNodeId];
-    }
+    if (aNodeId == this.mEndNode)
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 
     let map = JSON.parse(aNodeId);
 
-    let leeAlg = new alg2.Lee(map, InValidDirection);
+    console.log("\n");
+    alg3.CreateMatrix(JSON.parse(aNodeId)).Print();
+    console.log("---------------------------------------");
+
+    let leeAlg = new alg2.Lee(map, IsValidDirection);
 
     let amphipodsPositons = GetAmphipodsPositions(map);
 
@@ -124,26 +134,52 @@ class SpecialGraph {
             let costPos = { x: j, y: k };
             let cost = leeAlg.GetCost(costPos);
 
-            if(aNodeId != startNode && IsEndPosition(pos, endPositions))
+            if (cost < 0)
               continue;
 
-            if (!IsNoPosition(costPos, map) && (cost > 0)) {
-              neighbours.push(CreateNode(map, simbol, amphipodIndex, pos, costPos, cost));
+            if(IsEndPosition(pos, endPositions))  {
+              let bottom = map[pos.y + 1][pos.x];
+              
+              if ((bottom == '#') || (bottom == simbol))
+                continue;
+            }
+
+            if (!IsNoPosition(costPos, endPositions, simbol)) {
+              
+              let newNode = CreateNode(map, simbol, amphipodIndex, pos, costPos, cost);
+              console.log();
+              alg3.CreateMatrix(JSON.parse(newNode.id)).Print();    
+              neighbours.push(newNode);
+
+             
             }
           }
       }
       else {
         for (let j = 0; j < endPositions.length; j++) {
           let costPos = endPositions[j];
+
           let cost = leeAlg.GetCost(costPos);
 
-          if (cost > 0)
-            neighbours.push(CreateNode(map, simbol, amphipodIndex, pos, costPos, cost));
+          if (cost > 0) {
+
+            let bottom = map[costPos.y + 1][costPos.x];
+
+          if (bottom != '#' && bottom != simbol)
+            continue;
+
+            let newNode = CreateNode(map, simbol, amphipodIndex, pos, costPos, cost);
+
+            //if (newNode.id == this.mEndNode) {
+              console.log();
+              alg3.CreateMatrix(JSON.parse(newNode.id)).Print();           
+            //}
+
+            neighbours.push(newNode);
+          }
         }
       }
     }
-
-    this.mCache[aNodeId] = neighbours;
 
     return neighbours;
   }
@@ -165,12 +201,14 @@ let map = util.MapInput('./Day23Input.txt', (aElem, aIndex) => {
   return line;
 }, '\r\n', this);
 
-console.log(map);
+//console.log(map);
 
 let startNode = JSON.stringify(map);
 let endNode = GenerateEndNode(map);
 
-let ss = new SpecialGraph(startNode);
+//alg3.CreateMatrix(JSON.parse(endNode)).Print();
+
+let ss = new SpecialGraph(startNode, endNode);
 /*let oo = ss.GetNeighbours(JSON.stringify(map));
 
 for (let i = 0; i < oo.length; i++) {
