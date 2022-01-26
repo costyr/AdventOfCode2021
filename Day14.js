71e064df4a530af02a9f471cde0eef86d5382c66
Costi Rada <costi.rada@caphyon.com>
2022-01-26 20:46:05 +0200

    Day 14 solution

--- Day14.js
+++ Day14.js
@@ -1,158 +1,114 @@
-const { count } = require('console');
 const util = require('./Util.js');
 
-function InvertRules(aInsts) {
-
-  let gg = []
-  for (let key in aInsts)
-  {
-    if (gg[aInsts[key]] === undefined)
-      gg[aInsts[key]] = [];
-    gg[aInsts[key]].push(key); 
-  } 
-  
-  return gg;
-}
-
-function CountOO(aTemplate) {
+function CountPolimer(aTemplate) {
   let oo = [];
   for (let i = 0; i < aTemplate.length; i++)
     if (oo[aTemplate[i]] === undefined)
-      oo[aTemplate[i]] = 0;
+      oo[aTemplate[i]] = 1;
     else
       oo[aTemplate[i]] += 1;
 
   let min = Number.MAX_SAFE_INTEGER;
   let max = 0;
 
-  for (let key in oo)
-  {
+  for (let key in oo) {
     if (oo[key] > max)
       max = oo[key];
-    
+
     if (oo[key] < min)
       min = oo[key];
   }
 
-  /*for (let key in oo)
-  {
-    oo[key] = Math.round(oo[key] * 100 / aTemplate.length);
-  }*/
-
-  console.log(oo);
-
   return max - min;
 }
 
-function PrintStats(aLine) {
-
-  let gg = [];
-  for (let i = 0; i < aLine.length; i++)
-    if (gg[aLine[i]] == undefined)
-      gg[aLine[i]] = 1;
-    else
-      gg[aLine[i]] += 1;
-
-  let ff = [];
-  for (let key in gg)
-  {
-    ff.push(Math.round(gg[key] * 100 / aLine.length)); 
-  }
-
-  return ff.sort((a, b) => { return a - b;});
-}
-
 function CreatePolimer(aTemplate, aInsts, aStepCount) {
-  
+
+  let template = util.CopyObject(aTemplate);
+
   let usedRules = [];
 
   for (let k = 0; k < aStepCount; k++) {
 
-  let partOO = [];
-  let line = '';  
-  for (let i = 0; i < aTemplate.length - 1; i++)
-  {
-    let pp = aTemplate[i] + aTemplate[i + 1];
+    let partOO = [];
+    let line = '';
+    for (let i = 0; i < template.length - 1; i++) {
+      let pp = template[i] + template[i + 1];
 
-    if (usedRules[pp] === undefined)
-      usedRules[pp] = 1;
-    else 
-      usedRules[pp] += 1;
-  
-    let nn = aInsts[pp];
+      if (usedRules[pp] === undefined)
+        usedRules[pp] = 1;
+      else
+        usedRules[pp] += 1;
 
-    if (partOO[nn] === undefined)
-      partOO[nn] = 1;
-    else
-    partOO[nn] += 1;
+      let nn = aInsts.get(pp);
 
-    if (nn !== undefined)
-    {
-      aTemplate.splice(i + 1, 0, nn);
-      line += nn;
-      i++;
+      if (partOO[nn] === undefined)
+        partOO[nn] = 1;
+      else
+        partOO[nn] += 1;
+
+      if (nn !== undefined) {
+        template.splice(i + 1, 0, nn);
+        line += nn;
+        i++;
+      }
     }
+  }
+  return CountPolimer(template);
+}
 
-    //let ff = CountOO(partOO);
+function CountPolimerBig(aInsts, aRules, aSteps) {
+
+  let map = new Map();
+
+  for (let i = 0; i < aInsts.length - 1; i++) {
+    let rr = aInsts[i] + aInsts[i + 1];
+    map.set(rr, (map.get(rr) ?? 0) + 1);
   }
 
-  //console.log(partOO);
-  //let ss = CountOO(aTemplate);
+  for (let i = 0; i < aSteps; i++) {
+    let newMap = new Map();
+    for (let [key, value] of map) {
+      let ff = aRules.get(key);
+      let aa = [];
+      aa[0] = key[0] + ff;
+      aa[1] = ff + key[1];
 
-  console.log(aTemplate.join(''));
-  //console.log(PrintStats(line) /*+ " " + ss + " " + tt + " " + aTemplate.join('') line*/);
+      for (let j = 0; j < aa.length; j++)
+        newMap.set(aa[j], (newMap.get(aa[j]) ?? 0) + value);
+    }
+    map = newMap;
+  }
+
+  let letterCount = [];
+  for (let [key, value] of map) {
+    for (let i = 0; i < key.length; i++) {
+      if (letterCount[key[i]] === undefined)
+        letterCount[key[i]] = value;
+      else
+        letterCount[key[i]] += value;
+    }
+  }
+
+  return Math.round((Math.max(...Object.values(letterCount)) / 2) - Math.min(...Object.values(letterCount)) / 2);
 }
 
-  //console.log(usedRules);
-
-  return CountOO(aTemplate);
-}
-
-let insts = util.MapInput('./Day14TestInput.txt', (aElem, aIndex) => {
+let insts = util.MapInput('./Day14Input.txt', (aElem, aIndex) => {
 
   if (aIndex == 0) {
     return aElem.split('');
   }
   else {
-    let rules = [];
-    aElem.split('\r\n').map(a => { let rr = a.split(' -> '); rules[rr[0]] = rr[1]; }, this);
+    let rules = new Map();
+    aElem.split('\r\n').map(a => { let rr = a.split(' -> '); rules.set(rr[0], rr[1]); }, this);
 
     return rules;
   }
 
 }, '\r\n\r\n');
 
-console.log(insts);
+console.log(CreatePolimer(insts[0], insts[1], 10));
 
-console.log(insts[0].join(''));
+let total = CountPolimerBig(insts[0], insts[1], 40);
 
-let invInsts = InvertRules(insts[1]);
-
-console.log(invInsts);
-
-for (let key in insts[1]) {
-  console.log(key + "=>" + insts[1][key])
-let ret = CreatePolimer([key[0], [key[1]]], insts[1], 7);
-
-console.log(ret);
-}
-
-/*ret = CreatePolimer(['N', 'N'], insts[1], 10);
-
-console.log(ret);
-
-ret = CreatePolimer(['N', 'C'], insts[1], 10);
-
-console.log(ret);
-
-ret = CreatePolimer(['C', 'B'], insts[1], 10);
-
-console.log(ret);*/
-
-/*let paper = MarkPoints(insts[0], max);
-
-let firstFoldCount = Fold(paper, insts[1], insts[1].length);
-
-console.log(firstFoldCount);
-
-paper.Print('', (aElem) => { if (aElem == '.') return ' '; else return '*'; });*/
+console.log(total);
